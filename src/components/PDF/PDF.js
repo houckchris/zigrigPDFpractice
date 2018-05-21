7aee18e658fd5d6df7696bdbc6d06c2c9f15ae44
Chris Houck <chrishouck@Chriss-MacBook-Pro.local>
2018-05-21 10:48:42 -0400

    Template PDF

--- src/components/PDF/PDF.js
+++ src/components/PDF/PDF.js
@@ -5,10 +5,67 @@ import jsPDF from "jspdf";
 
 var pdfData;
 var pdfFileName;
+var pdfHTML = 
+`<div>
+    <h1>ZigRig Run Diagram</h1>
+    <h2>T.Novell Diagram #1014</h2>
+    <table>
+        <tr>
+            <th>Draws</th>
+            <th>Voltage</th>
+            <th>Current</th>
+            <th>Leg 1</th>
+            <th>Leg 2</th>
+            <th>Leg 3</th>
+        </tr>
+        <tr>
+            <td>Rooftop 20k #1</td>
+            <td>120/208V</td>
+            <td>166.6</td>
+            <td>83.3A</td>
+            <td>83.3A</td>
+            <td>-----</td>
+        </tr>
+        <tr>
+            <td>Rooftop 20k #2</td>
+            <td>120/208V</td>
+            <td>166.6</td>
+            <td>83.3A</td>
+            <td>-----</td>
+            <td>83.3A</td>
+        </tr>
+        <tr>
+            <td>Rooftop 20k #3</td>
+            <td>120/208V</td>
+            <td>166.6</td>
+            <td>-----</td>
+            <td>83.3A</td>
+            <td>83.3A</td>
+        </tr>
+        <tr>
+            <td>Rooftop 10k #3</td>
+            <td>120/208V</td>
+            <td>83.3A</td>
+            <td>41.65A</td>
+            <td>41.65A</td>
+            <td>-----</td>
+        </tr>
+        <tr>
+            <th>Totals</th>
+            <th>-----</th>
+            <th>583.1A</th>
+            <th>208.25A</th>
+            <th>208.25A</th>
+            <th>166.6A</th>
+        </tr>
+        
+    </table>
+</div>`
+;
 
 function HTMLtoPDF(){
     var pdf = new jsPDF('p', 'pt', 'letter');
-    var source = pdfData ||"<h1>Hello, world!</h1>";
+    var source = pdfData || pdfHTML;
     // source = $('#HTMLtoPDF')[0];
     var specialElementHandlers = {
         '#bypassme': function(element, renderer){
@@ -19,7 +76,24 @@ function HTMLtoPDF(){
         top: 50,
         left: 60,
         width: 545
-      };
+    };
+    // --------------------IMAGE INSERT--------------------
+    // function getBase64Image(img){
+    //     var canvas = document.createElement("canvas");
+    //     canvas.width = img.width;
+    //     canvas.height = img.height;
+    //     var ctx = canvas.getContext("2d");
+    //     ctx.drawImage(img, 0, 0);
+    //     var dataURL = canvas.toDataURL("images/ZigRig.png");
+    //     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    // };
+    // var img = new Image();
+    // img.onload = function(){
+    //     var dataURI = getBase64Image(img);
+    //     return dataURI;
+    // };
+    // img.src = "./images/ZigRig.png";
+    // pdf.addImage(img, 'png', 25, 50, 200, 200, 'zigrig');
     pdf.fromHTML(
           source // HTML string or DOM elem ref.
           , margins.left // x coord
